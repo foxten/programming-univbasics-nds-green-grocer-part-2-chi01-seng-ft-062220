084fc56ccbafa35020d8eefe1d2581afcc0c8b61
Tenaysia Fox <tenaysiafox@gmail.com>
2020-06-05 17:57:09 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -47,9 +47,10 @@ def checkout(cart, coupons)
   i = 0
     while i<clearance_applied.count do
       total += clearance_applied[i][:price]*clearance_applied[i][:count]
+       i +=1
+    end
     if total > 100
       total *= 0.9
-       i +=1
     end
 total
 end
