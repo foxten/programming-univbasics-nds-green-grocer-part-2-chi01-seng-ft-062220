0feb0127ef6fc76822eeebd123b524f7f742a0ab
Tenaysia Fox <tenaysiafox@gmail.com>
2020-06-05 17:10:57 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -28,9 +28,14 @@ cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+i=0
+  while i < cart.length
+    if cart[i][:clearance] == true
+      cart[i]
+    end
+    i+=1
+  end
+  cart
 end
 
 def checkout(cart, coupons)
