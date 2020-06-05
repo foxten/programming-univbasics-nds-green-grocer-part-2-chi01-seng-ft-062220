a909b08d5b1e03a84270206bcf30a6189703075c
Tenaysia Fox <tenaysiafox@gmail.com>
2020-06-05 16:58:58 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -15,7 +15,7 @@ def apply_coupons(cart, coupons)
       couponed_item = {
         :item => item_name
         :price => coupons[i][:cost]/coupons[i][:num],
-        :clearance => couponed_item[i][:clearance],
+        :clearance => couponed_item[:clearance],
         :count => coupons[i][:num],
       }
       cart << couponed_item
